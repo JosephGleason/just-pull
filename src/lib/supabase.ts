b89c695bc46bdc4b7e7545d87e7d717e8404dc26
Joseph Gleason <joeg1834@gmail.com>
2026-05-13 15:15:53 -0400

    fix: only skip AsyncStorage during SSR, not in browser

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import { Platform } from "react-native";
 
 const supabase_url = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabase_anon_key = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
@@ -8,16 +7,16 @@ let _supabase: SupabaseClient;
 
 function getSupabase(): SupabaseClient {
   if (!_supabase) {
-    const AsyncStorage =
-      Platform.OS === "web"
-        ? undefined
-        : require("@react-native-async-storage/async-storage").default;
+    const isSSR = typeof window === "undefined";
+    const AsyncStorage = isSSR
+      ? undefined
+      : require("@react-native-async-storage/async-storage").default;
 
     _supabase = createClient(supabase_url, supabase_anon_key, {
       auth: {
         storage: AsyncStorage,
         autoRefreshToken: true,
-        persistSession: true,
+        persistSession: !isSSR,
         detectSessionInUrl: false,
       },
     });
